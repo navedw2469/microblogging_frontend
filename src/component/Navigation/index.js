9b10bb091f1efdf3d1f2f63a3e20300b478a7b8e
Shabbir-poswal <shabbirposwal1@gmail.com>
2023-11-02 12:32:55 +0530

    enhancement

--- src/component/Navigation/index.js
+++ src/component/Navigation/index.js
@@ -1,63 +1,101 @@
-import React, { useState, useEffect, useContext } from 'react';
-import Image from 'next/image';
-import logo from "./images/logo.svg";
-import search from "./images/search.svg";
-import home from "./images/home.svg";
-import bell from "./images/bell.svg";
-import { useRouter } from 'next/navigation';
-import bookmark from './images/bookmark.svg';
-import profile from './images/profile.svg';
-import UserContext from '@/context/UserContext';
-import IconMapping from './IconMapping';
+import React, { useState, useEffect, useContext } from "react";
+import { useRouter } from "next/navigation";
+import UserContext from "@/context/UserContext";
+import IconMapping from "./IconMapping";
 
-const Navigation = () =>{
+const Navigation = () => {
   const router = useRouter();
   const { user } = useContext(UserContext);
   const [theme, setTheme] = useState();
-  console.log(user, 'user');
+  console.log(user, "user");
 
-  useEffect(() =>{
-    if(localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches))
-      setTheme('dark');
-    else
-      setTheme('light');
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    )
+      setTheme("dark");
+    else setTheme("light");
   }, []);
 
   const handleTheme = (toTheme) => {
     localStorage.theme = toTheme;
-    toTheme === 'dark' ? document.documentElement.classList.add('dark') : document.documentElement.classList.remove('dark');
+    toTheme === "dark"
+      ? document.documentElement.classList.add("dark")
+      : document.documentElement.classList.remove("dark");
     setTheme(toTheme);
-  }
+  };
 
   return (
-    <div className='xl:w-[250px] lg:x-[85px] lg:h-screen ml-3 mr-3 flex flex-col max-md:hidden overflow-y-scroll no-scrollbar dark:bg-slate-950'>
-      <div className='p-3'>
-        <svg className="h-10 w-10 dark:fill-white" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 112.77 122.88" xmlSpace="preserve"><g><path d="M64.44,61.11c1.79,0,3.12-1.45,3.12-3.12c0-1.78-1.45-3.12-3.12-3.12H24.75c-1.78,0-3.12,1.45-3.12,3.12 c0,1.78,1.45,3.12,3.12,3.12H64.44L64.44,61.11L64.44,61.11L64.44,61.11z M77.45,19.73l18.1-19.14c0.69-0.58,1.39-0.81,2.2-0.35 l14.56,14.1c0.58,0.69,0.69,1.5-0.12,2.31L93.75,36.14L77.45,19.73L77.45,19.73L77.45,19.73L77.45,19.73z M87.74,42.27l-18.66,3.86 l2.36-20.28L87.74,42.27L87.74,42.27z M19.14,13.09h41.73l-3.05,6.45H19.14c-3.48,0-6.65,1.43-8.96,3.73s-3.73,5.46-3.73,8.96 v45.74c0,3.48,1.43,6.66,3.73,8.96c2.3,2.3,5.47,3.73,8.96,3.73h3.72v0.01l0.21,0.01c1.77,0.12,3.12,1.66,2.99,3.43l-1.26,18.1 L48.78,97.7c0.58-0.58,1.38-0.93,2.27-0.93h37.32c3.48,0,6.65-1.42,8.96-3.73c2.3-2.3,3.73-5.48,3.73-8.96V50.45h6.68v42.69 c0.35,9.63-3.58,15.04-19.43,15.7l-32.25-0.74l-32.73,13.87l-0.16,0.13c-1.35,1.16-3.38,1-4.54-0.36c-0.57-0.67-0.82-1.49-0.77-2.3 l1.55-22.34h-0.26c-5.26,0-10.05-2.15-13.52-5.62C2.15,88.03,0,83.24,0,77.98V32.23c0-5.26,2.15-10.05,5.62-13.52 C9.08,15.24,13.87,13.09,19.14,13.09L19.14,13.09L19.14,13.09z M79.69,78.42c1.79,0,3.12-1.45,3.12-3.12 c0-1.79-1.45-3.12-3.12-3.12H24.75c-1.78,0-3.12,1.45-3.12,3.12c0,1.78,1.45,3.12,3.12,3.12H79.69L79.69,78.42L79.69,78.42 L79.69,78.42z M50.39,43.81c1.78,0,3.12-1.45,3.12-3.12c0-1.67-1.45-3.12-3.12-3.12H24.75c-1.78,0-3.12,1.45-3.12,3.12 c0,1.78,1.45,3.12,3.12,3.12H50.39L50.39,43.81L50.39,43.81L50.39,43.81z"/></g></svg>
+    <div className="xl:w-[250px] lg:x-[85px] lg:h-screen ml-3 mr-3 flex flex-col max-md:hidden overflow-y-scroll no-scrollbar dark:bg-slate-950">
+      <div className="p-3">
+        <svg
+          className="h-10 w-10 dark:fill-white"
+          version="1.1"
+          id="Layer_1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          x="0px"
+          y="0px"
+          viewBox="0 0 112.77 122.88"
+          xmlSpace="preserve"
+        >
+          <g>
+            <path d="M64.44,61.11c1.79,0,3.12-1.45,3.12-3.12c0-1.78-1.45-3.12-3.12-3.12H24.75c-1.78,0-3.12,1.45-3.12,3.12 c0,1.78,1.45,3.12,3.12,3.12H64.44L64.44,61.11L64.44,61.11L64.44,61.11z M77.45,19.73l18.1-19.14c0.69-0.58,1.39-0.81,2.2-0.35 l14.56,14.1c0.58,0.69,0.69,1.5-0.12,2.31L93.75,36.14L77.45,19.73L77.45,19.73L77.45,19.73L77.45,19.73z M87.74,42.27l-18.66,3.86 l2.36-20.28L87.74,42.27L87.74,42.27z M19.14,13.09h41.73l-3.05,6.45H19.14c-3.48,0-6.65,1.43-8.96,3.73s-3.73,5.46-3.73,8.96 v45.74c0,3.48,1.43,6.66,3.73,8.96c2.3,2.3,5.47,3.73,8.96,3.73h3.72v0.01l0.21,0.01c1.77,0.12,3.12,1.66,2.99,3.43l-1.26,18.1 L48.78,97.7c0.58-0.58,1.38-0.93,2.27-0.93h37.32c3.48,0,6.65-1.42,8.96-3.73c2.3-2.3,3.73-5.48,3.73-8.96V50.45h6.68v42.69 c0.35,9.63-3.58,15.04-19.43,15.7l-32.25-0.74l-32.73,13.87l-0.16,0.13c-1.35,1.16-3.38,1-4.54-0.36c-0.57-0.67-0.82-1.49-0.77-2.3 l1.55-22.34h-0.26c-5.26,0-10.05-2.15-13.52-5.62C2.15,88.03,0,83.24,0,77.98V32.23c0-5.26,2.15-10.05,5.62-13.52 C9.08,15.24,13.87,13.09,19.14,13.09L19.14,13.09L19.14,13.09z M79.69,78.42c1.79,0,3.12-1.45,3.12-3.12 c0-1.79-1.45-3.12-3.12-3.12H24.75c-1.78,0-3.12,1.45-3.12,3.12c0,1.78,1.45,3.12,3.12,3.12H79.69L79.69,78.42L79.69,78.42 L79.69,78.42z M50.39,43.81c1.78,0,3.12-1.45,3.12-3.12c0-1.67-1.45-3.12-3.12-3.12H24.75c-1.78,0-3.12,1.45-3.12,3.12 c0,1.78,1.45,3.12,3.12,3.12H50.39L50.39,43.81L50.39,43.81L50.39,43.81z" />
+          </g>
+        </svg>
       </div>
-      <div className='flex flex-col justify-between flex-1 dark:text-white'>
+      <div className="flex flex-col justify-between flex-1 dark:text-white">
         <div>
-          <IconMapping name={'Home'} href={'/posts'} />
-          <IconMapping name={'Find'} href={'/find'} />
-          <IconMapping name={'Notifications'} href={'/notifications'} />
-          <IconMapping name={'Bookmarks'} href={'/bookmarks'} />
-          <IconMapping name={'Profile'} href={`/${user?.user_name}`} />
-          <div className='p-3 mt-2 mr-10 bg-sky-500 rounded-full text-center font-bold text-white cursor-pointer hover:bg-blue-500 md:max-xl:hidden'>Post</div>
+          <IconMapping name={"Home"} href={"/posts"} />
+          <IconMapping name={"Find"} href={"/find"} />
+          <IconMapping name={"Notifications"} href={"/notifications"} />
+          <IconMapping name={"Bookmarks"} href={"/bookmarks"} />
+          <IconMapping name={"Profile"} href={`/${user?.user_name}`} />
+          <div className="p-3 mt-2 mr-10 bg-sky-500 rounded-full text-center font-bold text-white cursor-pointer hover:bg-blue-500 md:max-xl:hidden">
+            Post
+          </div>
         </div>
-        {theme === 'light' && <button type="button" className="bg-yellow-500" onClick={()=>handleTheme('dark')}>Dark Theme</button>}
-          {theme === 'dark' && <button type="button" className="bg-yellow-500" onClick={()=>handleTheme('light')}>Light Theme</button>}
-        <div className='p-3 pb-6 flex justify-between items-center md:max-xl:hidden'>
-          <div className='flex gap-2'>
-            <img src={user?.profile_image_url} alt='profile image' width="40" height="40" className='rounded-full'/>
+        {theme === "light" && (
+          <button
+            type="button"
+            className="bg-yellow-500"
+            onClick={() => handleTheme("dark")}
+          >
+            Dark Theme
+          </button>
+        )}
+        {theme === "dark" && (
+          <button
+            type="button"
+            className="bg-yellow-500"
+            onClick={() => handleTheme("light")}
+          >
+            Light Theme
+          </button>
+        )}
+        <div className="p-3 pb-6 flex justify-between items-center md:max-xl:hidden">
+          <div className="flex gap-2">
+            <img
+              src={user?.profile_image_url}
+              alt="profile image"
+              width="40"
+              height="40"
+              className="rounded-full"
+            />
             <div>
-              <p className='font-bold text-[15px]'>{user?.full_name}</p>
-              <p className='font-light text-[12px]'>@{user?.user_name}</p>
+              <p className="font-bold text-[15px]">{user?.full_name}</p>
+              <p className="font-light text-[12px]">@{user?.user_name}</p>
             </div>
           </div>
-          <div className='font-bold text-red-400 cursor-pointer hover:text-red-500'>Log Out</div>
+          <div className="font-bold text-red-400 cursor-pointer hover:text-red-500">
+            Log Out
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navigation;
--- src/component/User/index.js
+++ src/component/User/index.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import Card from "../Card"
-import Navigation from '../Navigation';
-import RightSection from '../RightSection';
-import linkIcon from './link_icon.png'
-import back from './back.png'
-import Image from 'next/image'
-import { useRouter } from 'next/router';
-import secureAPI from '@/api/axios';
+import React, { useState, useEffect } from "react";
+import Card from "../Card";
+import Navigation from "../Navigation";
+import RightSection from "../RightSection";
+import linkIcon from "./link_icon.png";
+import back from "./back.png";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import secureAPI from "@/api/axios";
 
 const isEmpty = (obj) => {
   for (const prop in obj) {
@@ -16,137 +16,206 @@ const isEmpty = (obj) => {
   }
 
   return true;
-}
+};
 
 const User = () => {
   const router = useRouter();
-  const userName = router.query.user_name
+  const userName = router.query.user_name;
 
-  const [tab, setTab] = useState('posts');
+  const [tab, setTab] = useState("posts");
   const [followed, setFollowed] = useState(false);
   const [data, setData] = useState({});
   const [posts, setPosts] = useState({});
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    secureAPI.get("get_user", {
-      params: { user_name: userName },
-    }).then(function (response) {
-      setData(response.data?.data);
-    }).catch(function (error) {
-      console.log(error);
-    });
+    secureAPI
+      .get("get_user", {
+        params: { user_name: userName },
+      })
+      .then(function (response) {
+        setData(response.data?.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }, [userName]);
 
-  useEffect(()=>{
-    if(!isEmpty(data)){
+  useEffect(() => {
+    if (!isEmpty(data)) {
       setLoading(true);
-      secureAPI.get("list_posts", {
-        params: { is_user_data_required: true },
-      }).then(function (response) {
-        setPosts(response.data);
-        setLoading(false);
-      }).catch(function (error) {
-        console.log(error);
-        setLoading(false);
-      });
+      secureAPI
+        .get("list_posts", {
+          params: { is_user_data_required: true },
+        })
+        .then(function (response) {
+          setPosts(response.data);
+          setLoading(false);
+        })
+        .catch(function (error) {
+          console.log(error);
+          setLoading(false);
+        });
     }
-  },[data, tab])
+  }, [data, tab]);
 
   const handleScroll = (e) => {
-    const bottom = (e.target.scrollHeight - e.target.scrollTop) <= e.target.clientHeight + 1;
+    const bottom =
+      e.target.scrollHeight - e.target.scrollTop <= e.target.clientHeight + 1;
 
     if (bottom && !loading && posts?.page !== posts?.total) {
       setLoading(true);
-      secureAPI.get("list_posts", {
-        params: { is_user_data_required: true, page: posts.page + 1 },
-      }).then(function (response) {
-        setPosts((oldData)=>{
-          return { ...response.data, list: [...oldData.list, ...response.data.list] };
+      secureAPI
+        .get("list_posts", {
+          params: { is_user_data_required: true, page: posts.page + 1 },
+        })
+        .then(function (response) {
+          setPosts((oldData) => {
+            return {
+              ...response.data,
+              list: [...oldData.list, ...response.data.list],
+            };
+          });
+          setLoading(false);
+        })
+        .catch(function (error) {
+          console.log(error);
+          setLoading(false);
         });
-        setLoading(false);
-      }).catch(function (error) {
-        console.log(error);
-        setLoading(false);
-      });
     }
-  }
+  };
 
-  return(
-    <div className='flex justify-center'>
+  return (
+    <div className="flex justify-center">
       <Navigation />
-      <div className="w-[625px] h-[100vh] overflow-y-auto border-r border-l" onScroll={handleScroll}>
+      <div
+        className="w-[625px] h-[100vh] overflow-y-auto border-r border-l"
+        onScroll={handleScroll}
+      >
         <div className="p-3 gap-4 flex items-center sticky top-0 bg-opacity-80 bg-white backdrop-filter backdrop-blur-md">
-          <Image src={back} alt="" className="h-[40px] w-[40px] rounded-full hover:bg-gray-200 cursor-pointer" onClick={()=>router.back()}/>
+          <Image
+            src={back}
+            alt=""
+            className="h-[40px] w-[40px] rounded-full hover:bg-gray-200 cursor-pointer"
+            onClick={() => router.back()}
+          />
           <div>
             <div className="text-[20px]">{data?.user_name}</div>
             <div className="text-[12px] text-gray-700">@{data?.full_name}</div>
           </div>
         </div>
-        <div className='flex p-10 gap-5'>
+        <div className="flex p-10 gap-5">
           <img
             src={data?.profile_image_url}
-            alt='profile Image'
-            className='w-[30%] h-[0%] rounded-full bg-slate-800'
+            alt="profile Image"
+            className="w-[30%] h-[0%] rounded-full bg-slate-800"
           />
           <div className="flex-1">
-            <div className='flex items-center gap-3 mb-3 justify-between'>
+            <div className="flex items-center gap-3 mb-3 justify-between">
               <div>
-                <div className='text-[24px]'>{data?.full_name}</div>
-                <div className='text-[14px] text-gray-700 font-light'>@{data?.user_name}</div>
+                <div className="text-[24px]">{data?.full_name}</div>
+                <div className="text-[14px] text-gray-700 font-light">
+                  @{data?.user_name}
+                </div>
               </div>
-              {!followed && <div className='px-4 py-2 border-2 rounded-full bg-blue-400 text-white font-bold hover:bg-blue-500 cursor-pointer' onClick={()=>setFollowed(true)}> Follow </div>}
-              {followed && <div className='px-4 py-2 border-2 rounded-full font-bold cursor-pointer hover:bg-slate-100' onClick={()=>setFollowed(false)}> Unfollow </div>}
+              {!followed && (
+                <div
+                  className="px-4 py-2 border-2 rounded-full bg-blue-400 text-white font-bold hover:bg-blue-500 cursor-pointer"
+                  onClick={() => setFollowed(true)}
+                >
+                  {" "}
+                  Follow{" "}
+                </div>
+              )}
+              {followed && (
+                <div
+                  className="px-4 py-2 border-2 rounded-full font-bold cursor-pointer hover:bg-slate-100"
+                  onClick={() => setFollowed(false)}
+                >
+                  {" "}
+                  Unfollow{" "}
+                </div>
+              )}
             </div>
-            <div className='flex gap-5 mb-1'>
-              <div className='flex hover:underline cursor-pointer' onClick={()=>router.push(`/${userName}/followers`)}>
-                <div className='text-[14px] font-bold'>{234}</div>
-                <div className='text-[14px]'>&nbsp;{"Followers"}</div>
+            <div className="flex gap-5 mb-1">
+              <div
+                className="flex hover:underline cursor-pointer"
+                onClick={() => router.push(`/${userName}/followers`)}
+              >
+                <div className="text-[14px] font-bold">{234}</div>
+                <div className="text-[14px]">&nbsp;{"Followers"}</div>
               </div>
-              <div className='flex gap-1 hover:underline cursor-pointer' onClick={()=>router.push(`/${userName}/following`)}>
-                <div className='text-[14px] font-bold'>{234}</div>
-                <div className='text-[14px]'>&nbsp;{"Followings"}</div>
+              <div
+                className="flex gap-1 hover:underline cursor-pointer"
+                onClick={() => router.push(`/${userName}/following`)}
+              >
+                <div className="text-[14px] font-bold">{234}</div>
+                <div className="text-[14px]">&nbsp;{"Followings"}</div>
               </div>
             </div>
-            <div className='mb-1'>{data?.bio}</div>
+            <div className="mb-1">{data?.bio}</div>
             {/* <div className='flex gap-1'>
               <Image src={linkIcon} className='w-[16px]'/>
               <div>{}</div>
             </div> */}
           </div>
         </div>
-        <div className='flex border-b'>
-          <div className='flex-1 hover:bg-slate-200 cursor-pointer' onClick={()=>{setTab("posts")}}>
-            <div className='text-center p-2 font-bold'>Posts</div>
-            { tab === 'posts' && <div className='h-[5px] bg-blue-400 rounded-md ml-12 mr-12'></div>}
-          </div>
-          
-          <div className='flex-1 hover:bg-slate-200 cursor-pointer' onClick={()=>{setTab("replies")}}>
-            <div className='text-center p-2 font-bold'>Replies</div>
-            { tab === 'replies' && <div className='h-[5px] bg-blue-400 rounded-md ml-12 mr-12'></div>}
-          </div>
-
-          <div className='flex-1 hover:bg-slate-200 cursor-pointer' onClick={()=>{setTab("media")}}>
-            <div className='text-center p-2 font-bold'>Media</div>
-            { tab === 'media' && <div className='h-[5px] bg-blue-400 rounded-md ml-12 mr-12'></div>}
+        <div className="flex border-b">
+          <div
+            className="flex-1 hover:bg-slate-200 cursor-pointer"
+            onClick={() => {
+              setTab("posts");
+            }}
+          >
+            <div className="text-center p-2 font-bold">Posts</div>
+            {tab === "posts" && (
+              <div className="h-[5px] bg-blue-400 rounded-md ml-12 mr-12"></div>
+            )}
           </div>
 
-          <div className='flex-1 hover:bg-slate-200 cursor-pointer' onClick={()=>{setTab("likes")}}>
-            <div className='text-center p-2 font-bold'>Likes</div>
-            { tab === 'likes' && <div className='h-[5px] bg-blue-400 rounded-md ml-12 mr-12'></div>}
+          <div
+            className="flex-1 hover:bg-slate-200 cursor-pointer"
+            onClick={() => {
+              setTab("replies");
+            }}
+          >
+            <div className="text-center p-2 font-bold">Replies</div>
+            {tab === "replies" && (
+              <div className="h-[5px] bg-blue-400 rounded-md ml-12 mr-12"></div>
+            )}
+          </div>
+
+          <div
+            className="flex-1 hover:bg-slate-200 cursor-pointer"
+            onClick={() => {
+              setTab("media");
+            }}
+          >
+            <div className="text-center p-2 font-bold">Media</div>
+            {tab === "media" && (
+              <div className="h-[5px] bg-blue-400 rounded-md ml-12 mr-12"></div>
+            )}
+          </div>
+
+          <div
+            className="flex-1 hover:bg-slate-200 cursor-pointer"
+            onClick={() => {
+              setTab("likes");
+            }}
+          >
+            <div className="text-center p-2 font-bold">Likes</div>
+            {tab === "likes" && (
+              <div className="h-[5px] bg-blue-400 rounded-md ml-12 mr-12"></div>
+            )}
           </div>
         </div>
-        {
-        posts?.list?.map((obj)=>{
-          return(
-            <Card key={obj?.id} data={obj} />
-            );
-          })
-        }
+        {posts?.list?.map((obj) => {
+          return <Card key={obj?.id} data={obj} />;
+        })}
       </div>
       <RightSection />
     </div>
-  )
-}
+  );
+};
 
 export default User;
